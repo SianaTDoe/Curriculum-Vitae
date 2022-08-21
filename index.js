c6c681116b0ff4f520ef07569257080d96e3a3a7
FlorianPrudence <florian.prudence@altazion.com>
2022-08-21 23:49:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+///<reference path="scripts/angularjs/jquery.d.ts"/>
+///<reference path="scripts/angularjs/angular-animate.d.ts" />
+///<reference path="scripts/angularjs/angular.d.ts" />
+var Index;
+(function (Index) {
+    var DefaultPage = /** @class */ (function () {
+        function DefaultPage($scope) {
+            this.scope = $scope;
+            var self = this;
+            self.scope.events = self;
+            self.init();
+        }
+        DefaultPage.prototype.init = function () {
+            var self = this.scope;
+            var method = this;
+            console.log("test");
+            self.detailsCreo = false;
+            self.detailsComdata = false;
+            self.detailsCNRS = false;
+        };
+        DefaultPage.$inject = ["$scope"];
+        return DefaultPage;
+    }());
+    Index.DefaultPage = DefaultPage;
+})(Index || (Index = {}));
+var IndexApp = angular.module('IndexApp', ['ngAnimate', 'ngSanitize']);
+IndexApp.controller('IndexController', Index.DefaultPage);
---
+++ index.ts
@@ -0,0 +1,37 @@
+﻿///<reference path="scripts/angularjs/jquery.d.ts"/>
+///<reference path="scripts/angularjs/angular-animate.d.ts" />
+///<reference path="scripts/angularjs/angular.d.ts" />
+
+module Index {
+
+    export interface IDefaultPageScope extends ng.IScope {
+        events: DefaultPage;
+        detailsCreo: boolean; 
+        detailsComdata: boolean;
+        detailsCNRS: boolean;
+    }
+
+    export class DefaultPage {
+        scope: IDefaultPageScope;
+        static $inject = ["$scope"];
+        constructor($scope: IDefaultPageScope) {
+            this.scope = $scope;
+            var self = this;
+            self.scope.events = self;
+            self.init();
+        }
+
+        private init() {
+            var self = this.scope;
+            var method = this; 
+            console.log("test");
+            self.detailsCreo = false;
+            self.detailsComdata = false;
+            self.detailsCNRS = false;
+        }
+    }
+}
+
+
+var IndexApp = angular.module('IndexApp', ['ngAnimate', 'ngSanitize']);
+IndexApp.controller('IndexController', Index.DefaultPage);
